307e281f55d7c750abfca7833b9987f31c631f8a
itssagarfiverr <77827340+itssagarfiverr@users.noreply.github.com>
2024-07-29 11:58:47 +0530

    Delete aaPanel.sh

--- aaPanel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clear
-sudo apt update -y
-URL=https://www.aapanel.com/script/install_7.0_en.sh && if [ -f /usr/bin/curl ];then curl -ksSO "$URL" ;else wget --no-check-certificate -O install_7.0_en.sh "$URL";fi;bash install_7.0_en.sh aapanel
-
-# Remove startup script from .bashrc
-sed -i "/wordpress_setup/d" ~/.bashrc
